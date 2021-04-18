acbf005dd02bd69337793c26dfe841aa8289ebff
Kaushalendra Pandey <pandeysid713@gmail.com>
2021-04-18 08:43:57 +0530

    product route completed

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,9 @@
 const Product = require('../models/product')
+const formidable = require("formidable")
+const _ = require("lodash")
+const fs = require('fs')
+const {check,validationResult} = require("express-validator")
+
 
 const getProductById = (req,res,next,id) => {
     Product.findById(id)
@@ -12,4 +17,152 @@ const getProductById = (req,res,next,id) => {
     })
 }
 
-module.exports = {getProductById}
+const createProduct = (req,res) => {
+
+    // const errors = validationResult(req)
+    // if(!errors.isEmpty()){
+    //     return res.status(400).json({error:errors.array()[0].msg})
+    // }
+
+    let form =  new formidable.IncomingForm()
+    form.keepExtensions = true
+
+    form.parse(req,(err,fields,file) => {
+        if(err){
+            res.status(400).json({
+                error:"Not a valid image!!!"
+            })
+        }
+
+        let product = new Product(fields)
+
+        //handle the file:
+        if(file.photo){
+            if(file.photo.size > 3*1024*1024){
+                return res.status(400).json({error:"File size too big!!!"})
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+
+        //save to the db:
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({error:"Saving the product failed.."})
+            }
+            res.json(product)
+        })
+
+    })
+
+}
+
+const getAllProducts = (req,res) => {
+    let limit = req.query.limit ? req.query.limit : 8
+    let sortBy = req.query.sortBy ? req.query.sortBy : ''
+
+    Product.find()
+    .populate("category")
+    .select("-photo")
+    .sort([[sortBy,"asc"]])
+    .limit(limit)
+    .exec((err,products)=>{
+        if(err || !products){
+            res.status(400).json({error:"No product exist!!!"})
+        }
+        res.json(products)
+    })
+}
+
+const getProduct = (req,res) => {
+    // this is done for optimization as loading image will take time.
+    // we will create a middleware to load image in background!
+    req.product.photo = undefined
+    res.json(req.product)
+}
+
+const updateProduct = (req,res) => {
+    let form =  new formidable.IncomingForm()
+    form.keepExtensions = true
+
+    form.parse(req,(err,fields,file) => {
+        if(err){
+            res.status(400).json({
+                error:"Not a valid image!!!"
+            })
+        }
+
+        //updation code...
+        let product = req.product
+        _.extend(product,fields)
+
+
+        //handle the file:
+        if(file.photo){
+            if(file.photo.size > 3*1024*1024){
+                return res.status(400).json({error:"File size too big!!!"})
+            }
+            product.photo.data = fs.readFileSync(file.photo.path)
+            product.photo.contentType = file.photo.type
+        }
+
+        //save to the db:
+        product.save((err,product)=>{
+            if(err){
+                res.status(400).json({error:"Updation failed.."})
+            }
+            res.json(product)
+        })
+
+    })
+}
+
+const deleteProduct = (req,res) => {
+    const product = req.product
+    product.remove((err,deletedProduct)=>{
+        if(err){
+            res.status(400).json({error:"Unable to delete the product.."})
+        }
+        res.json(deletedProduct)
+    })
+}
+
+const getAllUniqueCategories = (req,res) => {
+    Product.distinct("categories",(err,categories)=>{
+        if(err){
+            res.status(400).json({error:"No unique Category!!"})
+        }
+        res.json(categories)
+    })
+}
+
+//middleware
+const updateStock = (req,res,next) => {
+    let myOperations = req.body.order.products.map(prods => {
+        return {
+            updateOne:{
+                filter : {_id:prod._id},
+                update: {$inc :{stock :-prod.count ,sold : +prod.count}}
+            }
+        }
+    })
+
+    Product.bulkWrite(myOperations,(err,products)=>{
+        if(err){
+            res.status(400).json({error:"Bulk Opeartion failed"})
+        }
+        next()
+    })
+}
+
+
+
+module.exports = {getProductById,
+    createProduct,
+    getProduct,
+    deleteProduct,
+    updateProduct,
+    getAllProducts,
+    updateStock,
+    getAllUniqueCategories
+}
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,41 @@
 const express = require("express")
 const router = express.Router()
 const {isSignedIn,isAdmin,isAuthorized} = require('../controllers/auth')
-const {getProductById} = require("../controllers/product")
+const {getProductById,
+    createProduct,
+    getProduct,
+    updateProduct,
+    deleteProduct,
+    getAllProducts,
+    getAllUniqueCategories
+} = require("../controllers/product")
 const {getUserById} = require("../controllers/user")
+const {check,validationResult} = require("express-validator")
+
 
 //params
 router.param("userId",getUserById)
-router.param("productID",getProductById)
+router.param("productId",getProductById)
 
 //ROUTES:
 
+//CREATE
+router.post("/product/create/:userId",
+            check("name").isLength({min:5}).withMessage("Invalid Product Name"),
+            check("description").isLength({max:2000}).withMessage("Description too long!!"),
+            
+            isSignedIn,isAuthorized,isAdmin,createProduct)
 
+//READ:
+router.get("/product/:productId",getProduct)
+router.get("/products",getAllProducts)
+
+//UPDATE:
+router.put("/product/:productId/:userId",isSignedIn,isAuthorized,isAdmin,updateProduct)
+
+// DELETE:
+router.delete("/product/:productId/:userId",isSignedIn,isAuthorized,isAdmin,deleteProduct)
+
+router.get("/product/categories",getAllUniqueCategories)
 
 module.exports = router
